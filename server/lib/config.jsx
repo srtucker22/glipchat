cfa0c6fd5d2882ca992ef7f0f017ee4e14150b64
Simon Tucker <simontucker@Simons-MacBook-Air.local>
2015-10-19 16:45:56 -0700

    comment out facebook service

--- server/lib/config.jsx
+++ server/lib/config.jsx
@@ -3,11 +3,11 @@ ServiceConfiguration.configurations.remove({
   service: {$exists: true},
 });
 
-ServiceConfiguration.configurations.insert({
-  service: 'facebook',
-  appId: Meteor.settings.facebook.appId,
-  secret: Meteor.settings.facebook.secret,
-});
+// ServiceConfiguration.configurations.insert({
+//   service: 'facebook',
+//   appId: Meteor.settings.facebook.appId,
+//   secret: Meteor.settings.facebook.secret,
+// });
 
 ServiceConfiguration.configurations.insert({
   service: 'google',
