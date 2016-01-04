47c6cbb69755efeb5894a79da448157aec252b9a
Shane Coughlan <shane@opendawn.com>
2016-01-04 18:17:42 +0900

    Improved the commenting and messages for error handling
    Dev onboarding: added code comments.
    
    User Experience: Changed the user error text to remove contractions
    (difficult for non-native readers) and to improve context.

--- client/components/modules/error.component.jsx
+++ client/components/modules/error.component.jsx
@@ -1,3 +1,5 @@
+// SHANE: Adding some comments to give better context for developers.
+// SHANE: Changed the user error text to remove contractions (difficult for non-native readers) and to improve context.
 const {FontIcon} = MUI;
 const Colors = MUI.Styles.Colors;
 
@@ -57,6 +59,7 @@ Dependency.autorun(()=> {
   GlobalStyles = Dependency.get('GlobalStyles');
 });
 
+// Ask for permission to use the camera and microphone on the user's computer
 let permissionDeniedComponent = (appName)=> {
   return (
     <div style={[styles.permissionDenied.css]}>
@@ -81,33 +84,40 @@ let permissionDeniedComponent = (appName)=> {
   );
 };
 
+// Warn that the user is already connected to the room in a different window, tab or browser
 let duplicateErrorComponent = (
   <div className='row' style={[styles.general.css]}>
     <div className='col-xs-12 text-center'>
       <img src='/images/camel.png' style={[styles.general.icon.css]}/>
-      <p>Uh oh! You're already connected to this room in a different window, tab, or browser.</p>
+      <p>You are already connected to this room in a different window, tab, or browser.</p>
+      <p> To fix this problem you can try returning to the other window, tab or browser.</p>
     </div>
   </div>
 );
 
+// Warn that there has been a general error and ask the user to refresh the session
 let generalErrorComponent = (
   <div className='row' style={[styles.general.css]}>
     <div className='col-xs-12 text-center'>
       <img src='/images/atomic.png' style={[styles.general.icon.css]}/>
-      <p>Uh oh! Something went wrong. Please try refreshing the page.</p>
+        <p>Something went wrong. Please try refreshing the page.</p>
+        <p>If this does not fix the problem please close the tab or window and try again.</p>
     </div>
   </div>
 );
 
+// Alert the user that their browser is not supported and suggest they try Chrome or Firefox
 let notSupportedErrorComponent = (
   <div className='row' style={[styles.general.css]}>
     <div className='col-xs-12 text-center'>
       <img src='/images/astronaut.png' style={[styles.general.icon.css]}/>
-      <p>Sorry, we don't currently support your browser. Please use Chrome or Firefox.</p>
+        <p>Sorry, we do not currently support your browser.</p>
+        <p>You can download <a href="https://www.google.com/chrome/">Google Chrome</a> or <a href="https://www.mozilla.org/firefox">Mozilla Firefox</a> for free to use this video chatroom.</p>
     </div>
   </div>
 );
 
+// Logic for determining which error message to show when something goes wrong
 LocalStreamErrorComponent = Radium(React.createClass({
   render() {
     var {...other} = this.props;
