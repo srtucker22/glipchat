b925bf9efef72bde65db60f1d14b0ad719b5761f
Paul Cohen <paul@sonicviz.com>
2016-01-05 16:55:06 +0900

    Licence added and UX adjustments for testing
    Added license files throughout the project.
    
    Removed some commas flagged as unneeded by WebSphere in some files
    (noted in comments in each file)
    
    Biggest change to client/components/modules.controls.component.jsx
    needs testing to ensure it did not break:
    * Removed commas flagged as unneeded by WebSphere
     * Changed opacity to 0.5 for testing
     * Moved the button position for testing (added "top: '20%'" to const
    styles (reverted from commit c119164cd934f23cf17e0b4bc2fc83c966dfca15)

--- client/components/modules/video-overlay.component.jsx
+++ client/components/modules/video-overlay.component.jsx
@@ -1,27 +1,3 @@
-/**
- * quasar
- *
- * Copyright (c) 2015 Glipcode http://glipcode.com
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
- * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
- * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions
- * of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
- * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
- * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
- * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
- *
- * SHANE: Removed commas flagged as unneeded by WebSphere
- * SHANE: Changed opacity to 0.5 for testing
- *
- */
-
 let ding = new Audio('/audio/ding.mp3');
 
 const {FontIcon, FloatingActionButton, IconButton} = MUI;
@@ -35,7 +11,8 @@ const styles = {
     top: 0,
     width: '100%',
     zIndex: 1,
-    ':hover': {}
+
+    ':hover': {},
   },
 
   mute: {
@@ -43,15 +20,15 @@ const styles = {
     css: {
       backgroundColor: 'transparent',
       'float': 'right',
-      opacity: 0.5,
+      opacity: 0.3,
       position: 'absolute',
       right: '5px',
       top: '5px',
       transition: 'opacity 1s ease-in-out',
-      zIndex: 3
+      zIndex: 3,
     },
 
-    // styles for remote mute icon (peers mute themselves)
+    // styles for remote mute icon (peer mutes themself)
     remote: {
       css: {
         bottom: '5px',
@@ -59,15 +36,15 @@ const styles = {
         padding: 0,
         textShadow: '2px 2px rgba(0, 0, 0, 0.5)',
         top: 'initial',
-        width: '40px'
-      }
+        width: '40px',
+      },
     },
 
     visible: {
       css: {
-        opacity: 1
-      }
-    }
+        opacity: 1,
+      },
+    },
   },
 
   shade: {
@@ -77,21 +54,19 @@ const styles = {
       opacity: 0,
       transition: 'opacity 1s ease-in-out',
       width: '100%',
-      zIndex: 2
+      zIndex: 2,
     },
 
     hover: {
       css: {
-        opacity: 0.5
-      }
-    }
-  }
+        opacity: 0.5,
+      },
+    },
+  },
 };
 
-let RTCActions;
-RTCActions = null;
-let RTCStore;
-RTCStore = null;
+let RTCActions = null;
+let RTCStore = null;
 
 Dependency.autorun(()=> {
   RTCStore    = Dependency.get('RTCStore');
@@ -169,5 +144,5 @@ VideoOverlayComponent = Radium(React.createClass({
         </IconButton>
       </div>
     );
-  }
+  },
 }));
