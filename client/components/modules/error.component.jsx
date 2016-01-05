c119164cd934f23cf17e0b4bc2fc83c966dfca15
Shane Coughlan <shane@opendawn.com>
2016-01-05 12:14:37 +0900

    Licence added and UX adjustments for testing
    Added license files throughout the project.
    
    Removed some commas flagged as unneeded by WebSphere in some files
    (noted in comments in each file)
    
    Biggest change to client/components/modules.controls.component.jsx
    needs testing to ensure it did not break:
    * Removed commas flagged as unneeded by WebSphere
     * Changed opacity to 0.5 for testing
     * Moved the button position for testing (added "top: '20%'" to const
    styles

--- client/components/modules/error.component.jsx
+++ client/components/modules/error.component.jsx
@@ -1,6 +1,26 @@
-// SHANE: Adding some comments to give better context for developers.
-// SHANE: Changed the user error text to remove contractions (difficult for non-native readers) and to improve context.
-const {FontIcon} = MUI;
+/**
+ * quasar
+ *
+ * Copyright (c) 2015 Glipcode http://glipcode.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions
+ * of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+const FontIcon;
+FontIcon = MUI;
 const Colors = MUI.Styles.Colors;
 
 const styles = {
@@ -53,14 +73,16 @@ const styles = {
   },
 };
 
-let GlobalStyles = null;
+let GlobalStyles;
+GlobalStyles = null;
 
 Dependency.autorun(()=> {
   GlobalStyles = Dependency.get('GlobalStyles');
 });
 
 // Ask for permission to use the camera and microphone on the user's computer
-let permissionDeniedComponent = (appName)=> {
+let permissionDeniedComponent;
+permissionDeniedComponent = (appName)=> {
   return (
       <div style={[styles.permissionDenied.css]}>
         <div className='row'>
@@ -69,15 +91,17 @@ let permissionDeniedComponent = (appName)=> {
               GlobalStyles.cell,
               styles.permissionDenied.cell.css,
               styles.permissionDenied.arrow.css
-            ]} src='/images/arrow-left.png' />
+            ]} src='/images/arrow-left.png'/>
             <div style={[GlobalStyles.cell, styles.permissionDenied.cell.css]}>
-              Click the <FontIcon className='material-icons' style={styles.permissionDenied.icon.css} color={Colors.red500}>videocam_off</FontIcon> icon in the URL bar above to give {appName} access to your computer's camera and microphone.
+              Click the <FontIcon className='material-icons' style={styles.permissionDenied.icon.css}
+                                  color={Colors.red500}>videocam_off</FontIcon> icon in the URL bar above to
+              give {appName} access to your computer's camera and microphone.
             </div>
             <img style={[
               GlobalStyles.cell,
               styles.permissionDenied.cell.css,
               styles.permissionDenied.arrow.css
-            ]} src='/images/arrow-right.png' />
+            ]} src='/images/arrow-right.png'/>
           </div>
         </div>
       </div>
@@ -85,7 +109,8 @@ let permissionDeniedComponent = (appName)=> {
 };
 
 // Warn that the user is already connected to the room in a different window, tab or browser
-let duplicateErrorComponent = (
+let duplicateErrorComponent;
+duplicateErrorComponent = (
     <div className='row' style={[styles.general.css]}>
       <div className='col-xs-12 text-center'>
         <img src='/images/camel.png' style={[styles.general.icon.css]}/>
@@ -96,7 +121,8 @@ let duplicateErrorComponent = (
 );
 
 // Warn that there has been a general error and ask the user to refresh the session
-let generalErrorComponent = (
+let generalErrorComponent;
+generalErrorComponent = (
     <div className='row' style={[styles.general.css]}>
       <div className='col-xs-12 text-center'>
         <img src='/images/atomic.png' style={[styles.general.icon.css]}/>
@@ -107,12 +133,14 @@ let generalErrorComponent = (
 );
 
 // Alert the user that their browser is not supported and suggest they try Chrome or Firefox
-let notSupportedErrorComponent = (
+let notSupportedErrorComponent;
+notSupportedErrorComponent = (
     <div className='row' style={[styles.general.css]}>
       <div className='col-xs-12 text-center'>
         <img src='/images/astronaut.png' style={[styles.general.icon.css]}/>
         <p>Sorry, we do not currently support your browser.</p>
-        <p>You can download <a href="https://www.google.com/chrome/">Google Chrome</a> or <a href="https://www.mozilla.org/firefox">Mozilla Firefox</a> for free to use this video chatroom.</p>
+        <p>You can download <a href="https://www.google.com/chrome/">Google Chrome</a> or <a
+            href="https://www.mozilla.org/firefox">Mozilla Firefox</a> for free to use this video chatroom.</p>
       </div>
     </div>
 );
@@ -120,9 +148,12 @@ let notSupportedErrorComponent = (
 // Logic for determining which error message to show when something goes wrong
 LocalStreamErrorComponent = Radium(React.createClass({
   render() {
-    var {...other} = this.props;
+    var other;
+    var errorComponent;
 
-    var errorComponent = <div>{this.props.error.status}</div>;
+    other = this.props;
+
+    errorComponent = <div>{this.props.error.status}</div>;
 
     switch (this.props.error.status){
       case 'PermissionDeniedError':
--- client/stores/rtc.store.jsx
+++ client/stores/rtc.store.jsx
@@ -1,5 +1,5 @@
 /**
- * RTCStore Creator
+ * quasar
  *
  * Copyright (c) 2015 Glipcode http://glipcode.com
  *
