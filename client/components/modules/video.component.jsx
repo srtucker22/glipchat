8c0a398e6fc36c303d657204b1e45ea7e72a0bb8
Paul Cohen <paul@sonicviz.com>
2016-01-05 17:18:02 +0900

    Merge pull request #3 from shanecoughlan/master
    Licence added and UX adjustments for testing (reverted from commit 8875a39facb71861c8e4c7e215cd5c1279994997)

--- client/components/modules/video.component.jsx
+++ client/components/modules/video.component.jsx
@@ -1,46 +1,23 @@
-/**
- * quasar
- *
- * Copyright (c) 2015 Glipcode http://glipcode.com
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
- * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
- * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions
- * of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
- * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
- * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
- * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
- *
- * SHANE: Removed commas flagged as unneeded by WebSphere
- *
- */
-
 const styles = {
   css: {
     maxHeight: '100%',
     maxWidth: '100%',
     objectFit: 'cover',
-    transition: 'opacity 1s ease-in-out'
+    transition: 'opacity 1s ease-in-out',
   },
 
   flip: {
     css: {
-      transform: 'scale(-1, 1)'
-    }
+      transform: 'scale(-1, 1)',
+    },
   },
 
   fullScreen: {
     css: {
       height: '100%',
-      width: '100%'
-    }
-  }
+      width: '100%',
+    },
+  },
 };
 
 VideoComponent = Radium(React.createClass({
@@ -49,5 +26,5 @@ VideoComponent = Radium(React.createClass({
     return (
       <video style={[styles.css, this.props.flip && styles.flip.css, this.props.fullScreen && styles.fullScreen.css]} src={URL.createObjectURL(this.props.src)} muted={this.props.muted} autoPlay></video>
     );
-  }
+  },
 }));
